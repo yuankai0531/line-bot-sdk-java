90e5952b8a5ebb6daaba203392c0c0227a809307
yuankai0531 <42711005+yuankai0531@users.noreply.github.com>
2018-09-03 15:50:04 +0800

    Update EchoApplication.java

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -38,7 +38,7 @@ public class EchoApplication {
     public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
         System.out.println("event: " + event);
         final String originalMessageText = event.getMessage().getText();
-        return new TextMessage(originalMessageText);
+        return new TextMessage(originalMessageText+"mysub");
     }
 
     @EventMapping
